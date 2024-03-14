dd1488d5140c4c6fa317e02b0aab2e06b06d33eb
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-03-14 15:40:24 +0200

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ type-annotated sum of list function """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ returns sum of list elements """
     return float(sum(input_list))
