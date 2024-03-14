a460b28531b23d09339580377282dbf500f39453
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-03-14 16:20:15 +0200

    Update 7-to_kv.py

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """ type-annotated to_kv function """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """ returns a tuple """
     return (k, float(v**2))
