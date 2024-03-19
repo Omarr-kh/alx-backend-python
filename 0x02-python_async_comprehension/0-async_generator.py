ffcc2aa31d9a23792f1fd3782290163cc472d470
omar <64643650+Omarr-kh@users.noreply.github.com>
2024-03-19 13:46:50 +0200

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -9,4 +9,4 @@ async def async_generator() -> Generator[Float, None, None]:
     """ wait 1 sec then yield random number """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.random() * 10
